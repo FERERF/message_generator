33fc0e2b3ecfebb56006371036f0f2692c3f2074
Javon Haley <javonhaley202@gmail.com>
2024-09-17 10:17:21 -0500

    File added, currently has section one, two, and three finished, with the function underway.

---
+++ messagegenerator.js
@@ -0,0 +1,7 @@
+const sectionOne = ["As J. Wong once said", "Edgar Allen Poe once wrote", "Knock Knock", "Hunter Biden when Deer Biden walks in", "Why don't we trust HOI Players?"];
+const sectionTwo = ["He ain't Daigo!", "I became insane, with long intervals of horrible sanity.", "Whose there?", "Wait what scriptjava just walked in?", "Whaddya mean its not praxis to not trust them-?"];
+const sectionThree = ["Ok he might be Daigo...", "Fuck this nerd.", "Dude I rang your doorbell.", "GASP!", "Listen I'm not trusting some fuckin 4X player to finish the joke I don't care if it's not 'praxis'."];
+
+function messageMaker(sectionOne, sectionTwo, sectionThree) {
+
+};
