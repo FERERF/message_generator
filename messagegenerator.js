dbbe31e789b5fd3510e61ef1b3b8d65d48122343
Javon haley <40132223+FERERF@users.noreply.github.com>
2024-09-17 10:37:49 -0500

    Update messagegenerator.js

--- messagegenerator.js
+++ messagegenerator.js
@@ -2,6 +2,9 @@ const sectionOne = ["As J. Wong once said", "Edgar Allen Poe once wrote", "Knock
 const sectionTwo = ["He ain't Daigo!", "I became insane, with long intervals of horrible sanity.", "Whose there?", "Wait what scriptjava just walked in?", "Whaddya mean its not praxis to not trust them-?"];
 const sectionThree = ["Ok he might be Daigo...", "Fuck this nerd.", "Dude I rang your doorbell.", "GASP!", "Listen I'm not trusting some fuckin 4X player to finish the joke I don't care if it's not 'praxis'."];
 
-function messageMaker(sectionOne, sectionTwo, sectionThree) {
+function messageMaker() {
+  console.log(sectionOne[(Math.floor(Math.random() * sectionOne.length))]);
+  console.log(sectionTwo[(Math.floor(Math.random() * sectionTwo.length))]);
+  console.log(sectionThree[(Math.floor(Math.random() * sectionThree.length))]);
 
 };
